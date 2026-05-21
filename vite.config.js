9392c87a864cedc00d0cb13f2dc606b2da3c41ef
lucky52279 <lucky52279@qq.com>
2026-05-21 17:25:29 +0800

    修复网页空白

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [vue()],
   resolve: {
     alias: {
